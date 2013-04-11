7eeabd97d93e04478a034fc77a685fb536eff142
Ean Huddleston <ean.huddleston@gmail.com>
2013-04-10 17:49:23 -0700

    Make subscription selection possible when creating new user

--- app/controllers/subscription_plans_controller.rb
+++ app/controllers/subscription_plans_controller.rb
@@ -12,4 +12,17 @@ class SubscriptionPlansController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @subscription_plan = SubscriptionPlan.find(params[:id])
+  end
+
+  def update
+    @plan = SubscriptionPlan.find(params[:id])
+    if @plan.update_attributes(params[:subscription_plan])
+      redirect_to edit_subscription_plan_url(@plan)
+    else
+      render :edit
+    end
+  end
 end
